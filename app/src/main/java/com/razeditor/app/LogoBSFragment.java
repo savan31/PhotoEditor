4b82c09205110c04df9caf20514e49613d06d393
Savan Parvadiya <Sparvadiya7502@conestogac.on.ca>
2022-08-31 21:33:17 -0400

    Logo,Maks and properties Fragments are created

---
+++ app/src/main/java/com/razeditor/app/LogoBSFragment.java
@@ -0,0 +1,136 @@
+package com.razeditor.app;
+
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.BottomSheetDialogFragment;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class LogoBSFragment extends BottomSheetDialogFragment {
+
+    public LogoBSFragment() {
+        // Required empty public constructor
+    }
+
+    private LogoListener mStickerListener;
+
+    public void setLogoListener(LogoListener stickerListener) {
+        mStickerListener = stickerListener;
+    }
+
+    public interface LogoListener {
+        void onStickerClick(Bitmap bitmap);
+    }
+
+    private BottomSheetBehavior.BottomSheetCallback mBottomSheetBehaviorCallback = new BottomSheetBehavior.BottomSheetCallback() {
+
+        @Override
+        public void onStateChanged(@NonNull View bottomSheet, int newState) {
+            if (newState == BottomSheetBehavior.STATE_HIDDEN) {
+                dismiss();
+            }
+
+        }
+
+        @Override
+        public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+        }
+    };
+
+
+    @SuppressLint("RestrictedApi")
+    @Override
+    public void setupDialog(Dialog dialog, int style) {
+        super.setupDialog(dialog, style);
+        View contentView = View.inflate(getContext(), R.layout.fragment_bottom_sticker_emoji_dialog, null);
+        dialog.setContentView(contentView);
+        CoordinatorLayout.LayoutParams params = (CoordinatorLayout.LayoutParams) ((View) contentView.getParent()).getLayoutParams();
+        CoordinatorLayout.Behavior behavior = params.getBehavior();
+
+        if (behavior != null && behavior instanceof BottomSheetBehavior) {
+            ((BottomSheetBehavior) behavior).setBottomSheetCallback(mBottomSheetBehaviorCallback);
+        }
+        ((View) contentView.getParent()).setBackgroundColor(getResources().getColor(android.R.color.transparent));
+        RecyclerView rvEmoji = contentView.findViewById(R.id.rvEmoji);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 3);
+        rvEmoji.setLayoutManager(gridLayoutManager);
+        LogoAdapter stickerAdapter = new LogoAdapter();
+        rvEmoji.setAdapter(stickerAdapter);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+    }
+
+    public class LogoAdapter extends RecyclerView.Adapter<LogoAdapter.ViewHolder> {
+
+        int[] stickerList = new int[]{R.drawable.gt_logo,R.drawable.greenmeet_logo,R.drawable.l_a,R.drawable.l_b,R.drawable.l_c,R.drawable.l_d,R.drawable.l_e,R.drawable.l_f,R.drawable.l_g,
+                R.drawable.l_h,R.drawable.l_i,R.drawable.l_j,R.drawable.l_k,R.drawable.l_l,R.drawable.l_m,R.drawable.l_n,R.drawable.l_o,R.drawable.l_p,R.drawable.l_q,R.drawable.l_r};
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_sticker, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            holder.imgSticker.setImageResource(stickerList[position]);
+        }
+
+        @Override
+        public int getItemCount() {
+            return stickerList.length;
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+            ImageView imgSticker;
+
+            ViewHolder(View itemView) {
+                super(itemView);
+                imgSticker = itemView.findViewById(R.id.imgSticker);
+
+                itemView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (mStickerListener != null) {
+                            mStickerListener.onStickerClick(
+                                    BitmapFactory.decodeResource(getResources(),
+                                            stickerList[getLayoutPosition()]));
+                        }
+                        dismiss();
+                    }
+                });
+            }
+        }
+    }
+
+    private String convertEmoji(String emoji) {
+        String returnedEmoji = "";
+        try {
+            int convertEmojiToInt = Integer.parseInt(emoji.substring(2), 16);
+            returnedEmoji = getEmojiByUnicode(convertEmojiToInt);
+        } catch (NumberFormatException e) {
+            returnedEmoji = "";
+        }
+        return returnedEmoji;
+    }
+
+    private String getEmojiByUnicode(int unicode) {
+        return new String(Character.toChars(unicode));
+    }
+}
